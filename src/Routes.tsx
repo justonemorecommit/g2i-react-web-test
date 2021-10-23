515406aaf8947c3bbc2e1f6fa1ce7684181928a5
Jackson Zhang <91386299+justonemorecommit@users.noreply.github.com>
2021-10-23 05:20:26 -0400

    Added BeginContainer

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,10 +1,12 @@
 import { Switch, Route } from 'react-router'
 
+import Begin from './containers/Begin'
 import Question from './containers/Question'
 
 function Routes() {
   return (
     <Switch>
+      <Route exact path="/" component={Begin} />
       <Route path="/questions/:index" component={Question} />
     </Switch>
   )
---
+++ src/containers/Begin/BeginContainer.tsx
@@ -0,0 +1,38 @@
+import { useCallback } from 'react'
+import { RouteComponentProps } from 'react-router'
+import { Button, CardBody, CardFooter } from 'reactstrap'
+
+import AppCard from '../../components/AppCard'
+import AppContainer from '../../components/AppContainer'
+import './BeginContainer.styles.scss'
+
+type Props = RouteComponentProps
+
+function Begin(props: Props) {
+  const { history } = props
+
+  const handleClickBegin = useCallback(() => {
+    history.push('/questions/1')
+  }, [history])
+
+  return (
+    <div className="begin-container">
+      <AppContainer>
+        <AppCard className="begin-card">
+          <CardBody>
+            <h1 className="page-title">Welcome to the Trivia Challenge!</h1>
+            <p className="begin-text">
+              You will be presented with 10 True or False questions.
+            </p>
+            <p className="begin-text mb-5">Can you score 100%?</p>
+          </CardBody>
+          <CardFooter className="d-flex justify-content-center">
+            <Button onClick={handleClickBegin}>Begin</Button>
+          </CardFooter>
+        </AppCard>
+      </AppContainer>
+    </div>
+  )
+}
+
+export default Begin
