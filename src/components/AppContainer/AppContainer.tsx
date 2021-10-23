b00c849b89ed1a47c69d55ed41bc7c407aff8a7e
Jackson Zhang <91386299+justonemorecommit@users.noreply.github.com>
2021-10-23 05:16:45 -0400

    Added AppContainer component

---
+++ src/components/AppContainer/AppContainer.tsx
@@ -0,0 +1,17 @@
+import { Container, ContainerProps } from 'reactstrap'
+
+import './AppContainer.styles.scss'
+
+type Props = ContainerProps
+
+function AppContainer(props: Props) {
+  const { fluid, ...rest } = props
+  return (
+    <Container
+      {...rest}
+      className={fluid ? 'app-container-fluid' : 'app-container'}
+    />
+  )
+}
+
+export default AppContainer
